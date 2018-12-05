52b234c6dfdb0c37558d0b0b01dcf5545cc5b84d
Josh Schreuder <jschreuder@mediaocean.com>
2018-12-05 19:55:56 +1100

    Clean up solution 5a

--- 5/a.py
+++ 5/a.py
@@ -1,22 +1,16 @@
-from functools import reduce
-
-
 def react(polymer_fn):
     polymer = polymer_fn()
 
     i = 0
 
     while i < len(polymer) - 1:
-        #print("Processing", polymer[i], polymer[i+1])
         if polymer[i].upper() == polymer[i+1].upper() and polymer[i] != polymer[i+1]:
             polymer = polymer[:i] + polymer[i+2:]
-            # print("REACTION!")
             if i > 2:
                 i -= 2
             elif i >= 1:
                 i -= 1
         else:
             i += 1
-        #print("Up to character", i, "of", len(polymer))
 
     return len(polymer)
